324c80261ea0985f48bdef2f2f7ec5572a9f2c75
CDAMXI <cdachongm@gmail.com>
2025-09-01 13:28:33 +0200

    Refactor getCurrencyRates function to include Amount parameter for conversion calculation

--- CurrencyConverter/CurrencyConverter.py
+++ CurrencyConverter/CurrencyConverter.py
@@ -1,6 +1,6 @@
 import requests
 
-def getCurrencyRates(Base, Target):
+def getCurrencyRates(Base, Target, Amount):
     API_KEY = "5de90943678974951c39f487"          # pon aquí tu key
 
     url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{Base}"
@@ -11,10 +11,13 @@ def getCurrencyRates(Base, Target):
 
     if data["result"] != "success":
         raise RuntimeError(f"API error: {data.get('error-type', 'unknown')}")
-
-    rate = data["conversion_rates"][Target]  # EUR -> USD
-    print(f"1 {Base} = {rate} {Target}")
+    
+    rate = data["conversion_rates"][Target]
+    converted = Amount * data["conversion_rates"][Target]
+    print(f"{Amount} {Base} = {converted:.2f} {Target}")
 
 Base = input("From (currency code): ").upper()
+Amount = float(input("Amount: "))
 Target = input("To (currency code): ").upper()
-getCurrencyRates(Base, Target)
+
+getCurrencyRates(Base, Target, Amount)
