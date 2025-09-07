4b22a81aa671d914309e364ad255d6138ad9fb36
CDAMXI <cdachongm@gmail.com>
2025-09-07 11:29:44 +0200

    Enhance game method to initialize player display and board setup for Tic Tac Toe

--- TicTacToe/test.java
+++ TicTacToe/test.java
@@ -1,6 +1,28 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
 public class test {
     public static void main(String[] args) {
+        game();
+    }
+
+    public static void game() {
         Window window = new Window(900, 700, "Tic Tac Toe");
         window.display();
+        String[][] board = new String[3][3];
+        String[] players = {"X", "O"};
+        Random rand = new Random();
+        int currentPlayer = rand.nextInt(2); // Randomly select starting player
+
+        // First player
+        JPanel firstPlayer = new JPanel();
+        JLabel firstPlayerLabel = new JLabel();
+        firstPlayerLabel.setText("Current Player: " + players[currentPlayer]);
+        firstPlayer.add(firstPlayerLabel);
+
+        firstPlayerLabel.setFont(new Font("Arial", Font.BOLD, 20)); // Font size for player turn display
+        firstPlayer.add(firstPlayerLabel);
+        System.out.println();
     }
 }
